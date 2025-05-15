ba35a7633dda42f88f356b76b47b6a6f43b280f4
uiabu <ui.abukawsar@gmail.com>
2025-05-15 13:57:06 +0600

    Refactor code structure for improved readability and maintainability

--- components/HomePageSections/HomePageHeroSection.jsx
+++ components/HomePageSections/HomePageHeroSection.jsx
@@ -1,14 +1,44 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const HomePageHeroSection = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mt-10">
-        Welcome to Trip Law
-      </h1>
-      <p className="text-center mt-4">
-        Trip Law is dedicated to providing outstanding
-      </p>
+      <div className="relative">
+        <div className="">
+          {/* Background Image */}
+          <Image
+            src="/assets/homepage/home-banner.jpg" // Replace with your uploaded path if needed
+            alt="Immigration Clinic Banner"
+            width={1920}
+            height={1080}
+            className=""
+          />
+        </div>
+        {/* Overly Content */}
+        <div className="p-4 absolute bottom-0  transform   text-start ">
+          <h2 className="text-[#24C9FF] text-5xl font-bold">FREE</h2>
+          <h3 className="text-3xl font-semibold text-white">
+            IMMIGRATION CLINIC — <br />{" "}
+            <span className="text-2xl underline">
+              ONE-ON-ONE CONSULTATIONS!
+            </span>
+          </h3>
+        </div>
+      </div>
+      {/* bottom content */}
+      <div className="bg-[#24C9FF]">
+        <div className="container">
+          <div className="py-5">
+            <h2 className="text-xl font-bold text-start text-black">
+              GOT QUESTIONS ABOUT YOUR IMMIGRATION OPTIONS? GET ANSWERS FOR
+              FREE.
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

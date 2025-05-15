102963f3e2291d2fcf22d6ebb9f8cc54d2376136
uiabu <ui.abukawsar@gmail.com>
2025-05-15 16:54:25 +0600

    refactor: update text styles for consistency in Free Immigration Clinic component

--- components/HomePageSections/FreeImmigrationClinic.jsx
+++ components/HomePageSections/FreeImmigrationClinic.jsx
@@ -9,11 +9,11 @@ const FreeImmigrationClinic = () => {
           <h2 className="text-[18px] lg:text-[22px] text-[#060B28] font-bold lg:font-extrabold ">
             🛂 Free Immigration Clinic
           </h2>
-          <p className="text-[16px] text-gray-700 font-semibold mt-2 mb-4">
+          <p className="text-[16px] text-gray-700  mt-2 mb-4">
             One-on-One Consultations with Attorney Hardam H. Tripathi, Esq.
             Hosted by Trip Law, P.A. | Powered by Social Midas
           </p>
-          <ul className="text-[16px] text-[#060B28] font-semibold mt-2 space-y-3">
+          <ul className="text-[16px] text-[#060B28] font-medium mt-2 space-y-3">
             <li>
               <span>📅</span> Saturday, May 24, 2025
             </li>
@@ -33,7 +33,7 @@ const FreeImmigrationClinic = () => {
           <h2 className="text-[18px] lg:text-[22px] text-[#060B28] font-bold lg:font-extrabold ">
             📝 Fill Out the Case Info Form Below
           </h2>
-          <p className="text-[16px] text-gray-700 font-semibold mt-2 mb-4">
+          <p className="text-[16px] text-gray-700  mt-2 mb-4">
             Save time by filling out the form before arriving. Uploading
             documents is optional but highly encouraged.
           </p>
